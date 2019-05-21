c072cb10f1147de01bde1563313f637cf470c42f
Unknown <ansc0098@stud.hs-kl.de>
2019-05-21 19:57:48 +0200

    neue Szene

---
+++ VirtualScienceLabProject/Assets/Scripts/NextScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Wechseln einer Szene (Pausetaste!)
+/// </summary>
+public class NextScene   : MonoBehaviour {
+    public System.String sceneName;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName.ToString());
+    }
+}
