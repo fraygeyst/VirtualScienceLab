667354683e8dae4a0254be822a6ea7e073fe86e3
Unknown <ansc0098@stud.hs-kl.de>
2019-07-12 18:23:59 +0200

    Waschbecken anpassung

---
+++ VirtualScienceLabProject/Assets/Scripts/DerBiegsameWasserstrahlScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerBiegsameWasserstrahlScript : MonoBehaviour {
+
+    private bool touchedPullover = false;
+    private bool touchedWaschbecken = false;
+    private GameObject pullover;
+    private GameObject waschbecken;
+
+	// Use this for initialization
+	void Start () {
+        pullover = GameObject.FindGameObjectWithTag("Pullover");
+        waschbecken = GameObject.FindGameObjectWithTag("Waschbecken");
+
+    }
+
+   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == pullover)
+        {
+            touchedPullover = true;
+            Debug.Log("Pullover mit Stab beruehrt");
+        }
+
+      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == waschbecken)
+        {
+            touchedWaschbecken = true;
+            Debug.Log("Stab im Waschbecken");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+          if (other.gameObject == waschbecken)
+        {
+            touchedWaschbecken = false; 
+            Debug.Log("Stab NICHT mehr im Waschbecken");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+       
+		
+	}
+
+   public bool isTouchedWaschbecken()
+    {
+        return touchedWaschbecken;
+    }
+
+    public bool isTouchedPullover()
+    {
+        return touchedPullover;
+    }
+}
