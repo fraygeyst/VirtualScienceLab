03e4bc49e07e88e69a0381d89731effc7000b346
fraygeyst <mazi0018stud.hs-kl.de>
2019-07-12 12:13:16 +0200

    Lab 3 funktioniert, Feuer outdoor

---
+++ VirtualScienceLabProject/Assets/Scripts/Licht/JustOneLight.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class JustOneLight : MonoBehaviour {
+
+    public GameObject pointLight;
+    private bool isOn;
+    private bool collisionHappend = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        isOn = pointLight.GetComponent<Light>().enabled;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collisionHappend = true;
+        Debug.Log("collisionHappend: " + collisionHappend);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collisionHappend = false;
+        Debug.Log("collisionHappend: " + collisionHappend);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.Trigger) && collisionHappend) || (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Trigger) && collisionHappend))
+        {
+            if (isOn)
+            {
+                pointLight.GetComponent<Light>().enabled = false;
+                isOn = false;
+            }
+            else
+            {
+                pointLight.GetComponent<Light>().enabled = true;
+                isOn = true;
+            }
+        }
+
+
+    }
+}
