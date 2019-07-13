fe3f9f8fe1692e2da9bee2ee36fe9c6dcf4f447c
Unknown <ansc0098@stud.hs-kl.de>
2019-07-13 19:47:33 +0200

    Lab3 versuch hinzugefügt/angepasst

---
+++ VirtualScienceLabProject/Assets/Scripts/Lab3TurnWaterlightOn.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class Lab3TurnWaterlightOn : MonoBehaviour {
+
+    public GameObject pointLight;
+    private bool isOn;
+    private bool collisionHappend = false;
+    public GameObject water;
+    public GameObject Gluehbirne;
+    public Material materialGluehbirneOFF;
+    public Material materialGluehbirneON;
+
+
+    // Use this for initialization
+    //Beim Start wird gepruepft ob das Licht an oder aus ist und entsprechend gesetzt
+    void Start()
+    {
+        
+    }
+
+    //sobald man sich z.B. mit dem Controller in dem Bereich befinden wird die Variable auf true gesetzt
+    private void OnTriggerEnter(Collider other)
+    {
+        collisionHappend = true;
+        Debug.Log("Lab3TurnWaterlightOn collisionHappend: " + collisionHappend);
+    }
+
+    //sobald man sich z.B. mit dem Controller aus dem Bereich entfernt wird die Variable auf false gesetzt
+    private void OnTriggerExit(Collider other)
+    {
+        collisionHappend = false;
+        Debug.Log("Lab3TurnWaterlightOn collisionHappend: " + collisionHappend);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+
+
+        //Sollte man sich in dem Bereich befinden und auf den Trigger Button druecken wird das Licht je nach Zustand an oder ausgeschlatet
+        if ((ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.Trigger) && collisionHappend) || (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Trigger) && collisionHappend))
+        {
+            if (isOn && water.GetComponent<Lab3WasserScript>().getIsSalzwasser())
+            {
+                pointLight.GetComponent<Light>().enabled = false;
+                isOn = false;
+                Gluehbirne.GetComponent<Renderer>().material = materialGluehbirneOFF;
+            }
+            else
+            {
+                //prüfen ob wasser leitet
+                if (water.GetComponent<Lab3WasserScript>().getIsSalzwasser())
+                {
+                    pointLight.GetComponent<Light>().enabled = true;
+                    isOn = true;
+                    Gluehbirne.GetComponent<Renderer>().material = materialGluehbirneON;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ VirtualScienceLabProject/Assets/Scripts/Lab3WasserScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab3WasserScript : MonoBehaviour
+{
+
+    private bool isSalzwasser = false;
+    public GameObject salzstein;
+    public GameObject stein1;
+    public GameObject stein2;
+    public GameObject nachricht;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+
+    //Coroutine um den Salzstein zu zersetzen
+    IEnumerator salzsteinZersetzen(int sec)
+    {
+        //6 sekunden warten
+        yield return new WaitForSeconds(sec);
+        Debug.Log("Salzstein im Wasserbecken in TriggerArea");
+           
+        //Salzstein obj zerstören, danach haben wir gut leitbares Salzwasser
+        Destroy(salzstein);
+        isSalzwasser = true;
+
+        //Nachricht einblenden
+        nachricht.SetActive(true);
+        StartCoroutine(nachrichtAusblenden(6));
+    }
+
+
+    //Nachricht soll nach ein paar sekunden wieder verschwinden
+    IEnumerator nachrichtAusblenden(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        nachricht.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject == salzstein)
+        {
+            //Salzstein soll sich nach 6 sekunden zersetzt haben
+            StartCoroutine(salzsteinZersetzen(6));
+           
+        }else if (col.gameObject == stein1)
+        {
+            Destroy(stein1);
+        }else if (col.gameObject == stein2)
+        {
+            Destroy(stein2);
+        }
+        
+    }
+
+    public bool getIsSalzwasser()
+    {
+        return isSalzwasser;
+    }
+
+}
