6033555b272cdafa68d911574237851c91691c76
Unknown <ansc0098@stud.hs-kl.de>
2019-07-14 10:02:50 +0200

    Outdoorarea aufgehuebscht

--- VirtualScienceLabProject/Assets/Scripts/ChangeColorScript.cs
+++ VirtualScienceLabProject/Assets/Scripts/ChangeColorScript.cs
@@ -41,3 +41,53 @@ public class ChangeColorScript : MonoBehaviour {
         seconds = sec;
     }
 }
+
+/**
+ * public class ChangeColorScript : MonoBehaviour
+{
+
+    //Farbauswahl
+    public Material Materialcolor1;
+    public Material Materialcolor2;
+    private bool isMaterialcolor1;
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Erste Farbe setzten
+        gameObject.GetComponent<Renderer>().material = Materialcolor1;
+        isMaterialcolor1 = true;
+    }
+
+    void Update()
+    {
+       
+       
+        if (isMaterialcolor1)
+        {
+            
+            StartCoroutine(waitAndSetOtherMaterial(1, Materialcolor2));
+        }
+        else
+        {
+            StartCoroutine(waitAndSetOtherMaterial(1, Materialcolor1));
+
+        }
+       
+    }
+
+    IEnumerator waitAndSetOtherMaterial(int sec, Material m)
+    {
+        yield return new WaitForSeconds(sec);
+        //Ansonsten soll es kurz aufblinzeln
+       gameObject.GetComponent<Renderer>().material = m;
+        isMaterialcolor1 = !isMaterialcolor1;
+       
+    }
+}
+
+ * 
+ * 
+ * */
