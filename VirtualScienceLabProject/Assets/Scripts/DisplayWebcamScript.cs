7425f525e5b743ce7e3a774d97b5745d0f12cfd6
Unknown <ansc0098@stud.hs-kl.de>
2019-07-15 16:24:27 +0200

    anpassungen

---
+++ VirtualScienceLabProject/Assets/Scripts/DisplayWebcamScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayWebcamScript : MonoBehaviour {
+
+    public Camera cam;
+
+    void Start()
+    {
+        
+    }
+
+    // Take a "screenshot" of a camera's Render Texture.
+    Texture2D RTImage(Camera camera)
+    {
+        // The Render Texture in RenderTexture.active is the one
+        // that will be read by ReadPixels.
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = camera.targetTexture;
+
+        // Render the camera's view.
+        camera.Render();
+
+        // Make a new texture and read the active Render Texture into it.
+        Texture2D image = new Texture2D(camera.targetTexture.width, camera.targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, camera.targetTexture.width, camera.targetTexture.height), 0, 0);
+        image.Apply();
+
+        // Replace the original active Render Texture.
+        RenderTexture.active = currentRT;
+        return image;
+    }
+
+    private void Update()
+    {
+        cam.GetComponent<Renderer>().material.mainTexture = RTImage(cam);
+    }
+}
