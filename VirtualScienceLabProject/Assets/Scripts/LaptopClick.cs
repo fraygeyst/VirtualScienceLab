51a62f09074bbd4e029d38a1cbaf65ac471ebdf6
P. Lauer <philipp-lauer@t-online.de>
2019-09-11 19:16:20 +0200

    Basic 7 Bridge Problem

---
+++ VirtualScienceLabProject/Assets/Scripts/LaptopClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaptopClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject whale = GameObject.Find("humpback_whale");
+            whale.transform.Translate(0, 0, 1);
+        }
+    }
+}
