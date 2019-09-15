3b7b08ac945eef38004ffefeef448ad4723b7612
P. Lauer <philipp-lauer@t-online.de>
2019-09-15 18:29:02 +0200

    Added Scripts for 7 Bridges

---
+++ VirtualScienceLabProject/Assets/Scripts/Seven_Bridges.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seven_Bridges : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.name == "player")
+        {
+            switch (gameObject.name)
+            {
+                case "Bridge_1":
+                    bridge_1_act = true;
+                    bridgecount++;
+                    break;
+                case "Bridge_2":
+                    bridge_2_act = true;
+                    bridgecount++;
+                    break;
+                case "Bridge_3":
+                    bridge_3_act = true;
+                    bridgecount++;
+                    break;
+                case "Bridge_4":
+                    bridge_4_act = true;
+                    bridgecount++;
+                    break;
+                case "Bridge_5":
+                    bridge_5_act = true;
+                    bridgecount++;
+                    break;
+                case "Bridge_6":
+                    bridge_6_act = true;
+                    bridgecount++;
+                    break;
+                case "Bridge_7":
+                    bridge_7_act = true;
+                    bridgecount++;
+                    break;
+            }
+            Debug.Log("Bridgecount: " + gameObject.name + " - " + bridgecount);
+        }
+    }
+}
---
+++ VirtualScienceLabProject/Assets/Scripts/Seven_Bridges_Start.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seven_Bridges_Start : MonoBehaviour {
+
+    public static GameObject player;
+    public static bool start = true;
+    public static bool bridge_1_act;
+    public static bool bridge_2_act;
+    public static bool bridge_3_act;
+    public static bool bridge_4_act;
+    public static bool bridge_5_act;
+    public static bool bridge_6_act;
+    public static bool bridge_7_act;
+    public static int bridgecount;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "player")
+        {
+            if (start)
+            {
+                bridge_1_act = false;
+                bridge_2_act = false;
+                bridge_3_act = false;
+                bridge_4_act = false;
+                bridge_5_act = false;
+                bridge_6_act = false;
+                bridge_7_act = false;
+                bridgecount = 0;
+            }
+        }
+    }
+}
