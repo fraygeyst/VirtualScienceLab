8ab9e463dc638d1bcdfaeba2076f15ca8ecd3d3f
P. Lauer <philipp-lauer@t-online.de>
2019-10-16 13:32:14 +0200

    Seven Bridges

---
+++ VirtualScienceLabProject/Assets/Scripts/Seven_Bridges.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class Seven_Bridges : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
