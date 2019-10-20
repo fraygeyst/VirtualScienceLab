33db7fa198a3625fb1d8d19d8d7ff734ef974119
P. Lauer <philipp-lauer@t-online.de>
2019-10-20 18:01:53 +0200

    Fixed Camera Position Change based on Last Scene

--- VirtualScienceLabProject/Assets/Scripts/Scene_Management.cs
+++ VirtualScienceLabProject/Assets/Scripts/Scene_Management.cs
@@ -13,14 +13,14 @@ public class Scene_Management : MonoBehaviour {
         camera_obj = GameObject.Find("ViveRig");
         transform = camera_obj.GetComponent<Transform>();
         Vector3 vec = new Vector3();
-        vec = new Vector3(3.291f, 0f, 4.134f);
 
         if (scene.name == "NeuerFlur")
         {
+            Debug.Log("Switching Cam from: " + Globals.last_scene);
             switch (Globals.last_scene)
             {
                 case "BioLab":
-                    vec = new Vector3(3.291f, 0f, 4.134f);
+                    vec = new Vector3(12.0f, 0f, -1.4f);
                     break;
                 case "Elektro":
                     vec = new Vector3(3.291f, 0f, 4.134f);
@@ -41,21 +41,26 @@ public class Scene_Management : MonoBehaviour {
                     vec = new Vector3(3.291f, 0f, 4.134f);
                     break;
                 case "Teilchenlabor":
-                    vec = new Vector3(3.291f, 0f, 4.134f);
+                    vec = new Vector3(12.0f, 0f, 1.4f);
                     break;
                 case "VRLab":
-                    vec = new Vector3(3.291f, 0f, 4.134f);
+                    vec = new Vector3(16.9f, 0f, 1.4f);
                     break;
             }
         } else
         {
             Globals.last_scene = scene.name;
         }
-        camera_obj.transform.Translate(vec);
+        Debug.Log("Changing Position: " + vec);
+        camera_obj.transform.position = vec;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("Test: " + transform);
-	}
+
+    }
+
+    
+
+
 }
