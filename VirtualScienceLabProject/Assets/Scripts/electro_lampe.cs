a08fa6cf3eeffe6b9dcd6f42b6b39134b42a2851
P. Lauer <philipp-lauer@t-online.de>
2019-10-24 13:59:30 +0200

    Added Formulas for Volt and Ampere

---
+++ VirtualScienceLabProject/Assets/Scripts/electro_lampe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electro_lampe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private double getVolt(double x)
+    {
+        x = (-0.0490743420159 * Math.Pow(x, 6)) + (1.0680459493943 * Math.Pow(x, 5)) - (9.1705441592139 * Math.Pow(x, 4)) + (39.5654453080791 * Math.Pow(x, 3)) - (92.0907855870632 * Math.Pow(x, 2)) + (148.3179610169378 * x) + 0.1029496480702;
+        return x;
+    }
+
+    private double getAmpere(double x)
+    {
+        x = (0.0012809183618 * Math.Pow(x, 4)) - (0.019316637036 * Math.Pow(x, 3)) + (0.106348881997 * Math.Pow(x, 2)) + (0.6547898739782 * x) - 0.0044123552528;
+        return x;
+    }
+}
